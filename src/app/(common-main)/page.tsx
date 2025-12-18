b4701e9e846e37ec85d8901f13a3747f880809fc
King sejong <cestmoi7531@gmail.com>
2025-12-18 22:10:44 +0900

    webp 이미지 처리

--- src/app/(common-main)/page.tsx
+++ src/app/(common-main)/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useData } from '@/context/URLContext'
 import Image from 'next/image';
-import shilhouette from '/public/images/silhouette.webp'
+// import shilhouette from '/public/images/silhouette.webp'
 import Button from '@/components/button'
 
 export default function Home() {
@@ -18,13 +18,13 @@ export default function Home() {
 
   useEffect(() => {
     setData(window.location.href);
-  },[])
+  }, [])
 
   return (
     <>
       <div className={styles.main_wrap}>
         <p className={styles.sub_title}>나와 어울리는 영화 캐릭터는 누구일까??</p>
-        <Image className={styles.main_img} width={300} height={300} src={shilhouette} alt="메인 실루엣 이미지" />
+        <Image className={styles.main_img} width={300} height={300} src="/images/silhouette.webp" alt="메인 실루엣 이미지" />
       </div>
       <Button content={'시작하기'} onClick={enterQuestion} />
     </>
